b030ba985cd7402903c715ef1edf6d6d4e0e1337
marky edwards <mark@edwardsmark.com>
2022-06-21 14:20:31 -0700

    Update and rename sql-call.sql to mainGrid.sql

--- interface/mainGrid/mainGrid.sql
+++ interface/mainGrid/mainGrid.sql
@@ -1,4 +1,3 @@
-
 SET @LAYOUT_NAME    =  'PESL'   ;
 
 SELECT JSON_ARRAYAGG(
@@ -6,6 +5,7 @@ SELECT JSON_ARRAYAGG(
 		(	'stimwordPageNbr'	,	`stimword`.`stimwordPageNbr`
         ,   'stimwordLineNbr'	,	`stimword`.`stimwordLineNbr`
         ,   'stimwordWord'		,	`stimword`.`stimwordWord`
+        ,   'stimwordPhonetic'	,	`stimword`.`stimwordPhonetic`
 	  	)) AS ''   
 FROM    `stimword`
 WHERE   1
@@ -13,3 +13,7 @@ AND   `stimword`.`layoutName`       = @LAYOUT_NAME
 ORDER BY  `stimword`.`stimwordPageNbr`
 ,         `stimword`.`stimwordLineNbr`
 ;
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Securit
