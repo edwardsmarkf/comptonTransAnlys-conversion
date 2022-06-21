199df030c05c8e8afeabcd37b3b2f332b31484d9
marky edwards <mark@edwardsmark.com>
2022-06-21 13:56:03 -0700

    Update sql-call.sql

--- interface/mainGrid/sql-call.sql
+++ interface/mainGrid/sql-call.sql
@@ -1,25 +1,15 @@
 
+SET @LAYOUT_NAME    =  'PESL'   ;
 
-####   M A I N    S C R E E N    G R I D    C R E A T O R   ! ! !
- 
-        SET @LAYOUT_NAME    =  'PESL'   ;
-
-  SELECT DISTINCT ##JSON_ARRAYAGG(
-                JSON_OBJECT
-         	(	'stimwordPageNbr'	,	`stimword`.`stimwordPageNbr`
-                ,       'stimwordLineNbr'	,	`stimword`.`stimwordLineNbr`
-                ,       'stimwordWord'		,	`stimword`.`stimwordWord`
-	  	) AS ''
-                FROM    `stimword`
-		            ,       `stimwordPosition`
-                WHERE   1
-                AND	`stimword`.`layoutName`		    =	`stimwordPosition`.`layoutName`
-                AND	`stimword`.`stimwordPageNbr`	=	`stimwordPosition`.`stimwordPageNbr`
-                AND	`stimword`.`stimwordLineNbr`	=	`stimwordPosition`.`stimwordLineNbr`
-                AND	`stimword`.`stimwordWord`	    =	`stimwordPosition`.`stimwordWord`
-                AND `stimword`.`layoutName`       = @LAYOUT_NAME
-          #########################      AND     `stimword`.`stimwordPageNbr`                 BETWEEN  0 AND  1000
-                ORDER BY  `stimwordPosition`.`stimwordPageNbr`
-                ,         `stimwordPosition`.`stimwordLineNbr`
-                ;
-
+SELECT JSON_ARRAYAGG(
+		JSON_OBJECT
+		(	'stimwordPageNbr'	,	`stimword`.`stimwordPageNbr`
+        ,   'stimwordLineNbr'	,	`stimword`.`stimwordLineNbr`
+        ,   'stimwordWord'		,	`stimword`.`stimwordWord`
+	  	)) AS ''   
+FROM    `stimword`
+WHERE   1
+AND   `stimword`.`layoutName`       = @LAYOUT_NAME
+ORDER BY  `stimword`.`stimwordPageNbr`
+,         `stimword`.`stimwordLineNbr`
+;
