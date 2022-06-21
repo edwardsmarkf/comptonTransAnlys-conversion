7c31109c0b62fa72d20c077c8c7e4932dc5163ae
marky edwards <mark@edwardsmark.com>
2022-06-21 14:48:29 -0700

    Update mainGrid.sql

--- interface/mainGrid/mainGrid.sql
+++ interface/mainGrid/mainGrid.sql
@@ -1,19 +1,15 @@
 SET @LAYOUT_NAME    =  'PESL'   ;
 
 SELECT JSON_ARRAYAGG(
-		JSON_OBJECT
-		(	'stimwordPageNbr'	,	`stimword`.`stimwordPageNbr`
-        ,   'stimwordLineNbr'	,	`stimword`.`stimwordLineNbr`
-        ,   'stimwordWord'		,	`stimword`.`stimwordWord`
-        ,   'stimwordPhonetic'	,	`stimword`.`stimwordPhonetic`
-	  	)) AS ''   
+	JSON_OBJECT
+	(	'stimwordPageNbr'	,	`stimword`.`stimwordPageNbr`
+	,	'stimwordLineNbr'	,	`stimword`.`stimwordLineNbr`
+        ,	'stimwordWord'		,	`stimword`.`stimwordWord`
+        ,	'stimwordPhonetic'	,	`stimword`.`stimwordPhonetic`
+	)) AS ''   
 FROM    `stimword`
 WHERE   1
 AND   `stimword`.`layoutName`       = @LAYOUT_NAME
 ORDER BY  `stimword`.`stimwordPageNbr`
 ,         `stimword`.`stimwordLineNbr`
 ;
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Securit
