87cc51239e7e18c3be40419fcbd9ab90debc46cf
marky edwards <mark@edwardsmark.com>
2022-06-20 21:55:31 -0700

    Create clientStimword.sql

---
+++ interface/mainInputForm/clientStimword/clientStimword.sql
@@ -0,0 +1,27 @@
+        ## test display written 2022-02-18
+
+        SELECT  `clientContext`.`clientContextError`
+        ,       `clientContext`.`contextPosition`
+        ,       `clientContext`.`clientContextSpeakingErrors`
+        ,       `clientContext`.`clientContextErrorNotes`
+        ,       `clientContext`.`clientContextAutoIncr`
+        FROM    `comptonTransAnlys`.`clientContext`
+        WHERE   1
+        AND     `clientContext`.`contextAutoIncr`               =       56
+        AND     `clientContext`.`clientSessionAutoIncr`         =       2349
+        ;
+
+        SELECT  `clientContext`.`clientContextError`
+        ,       `clientStimword`.`stimwordWord`
+        ,       `clientStimword`.`contextPosition`
+        ,       `clientStimword`.`stimwordPositionSetting`
+        ,       `clientStimword`.`clientStimwordNotes`
+        ,       `clientStimword`.`clientStimwordAutoIncr`
+        ,       `clientStimword`.`stimwordPositionAutoIncr`
+        ,       `clientStimword`.`clientStimwordAutoIncr`
+        FROM    `comptonTransAnlys`.`clientContext`
+        LEFT JOIN `comptonTransAnlys`.`clientStimword`
+        ON      (`clientContext`.`clientContextAutoIncr`        =       `clientStimword`.`clientContextAutoIncr`)
+        WHERE   `clientContext`.`contextAutoIncr`               =       56
+        AND     `clientContext`.`clientSessionAutoIncr`         =       2349
+        ;
