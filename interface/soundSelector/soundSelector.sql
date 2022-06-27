abcee235cf3bed689e2753818671c4aa0ce3655e
marky edwards <mark@edwardsmark.com>
2022-06-27 10:23:27 -0700

    Update soundSelector.sql

--- interface/soundSelector/soundSelector.sql
+++ interface/soundSelector/soundSelector.sql
@@ -2,18 +2,40 @@
 
 SET @LAYOUT_NAME = 'PESL'  ;
 
-SELECT JSON_ARRAYAGG(JSON_OBJECT
-( 'soundTitle'		,	sound.soundTitle
-, 'soundSubtitle'	,	sound.soundSubTitle
-, 'soundPhoneme'	,	sound.soundPhoneme
-, 'contextPosition'	,	context.contextPosition
-, 'contextAutoIncr'	,	context.contextAutoIncr
-)) ''
-FROM comptonTransAnlys.sound
-, comptonTransAnlys.context
+SELECT #JSON_ARRAYAGG(JSON_OBJECT
+ 'soundTitle'			,	`sound_a`.`soundTitle`
+, 'soundSubtitle'		,	`sound_a`.`soundSubTitle`
+, 'soundSubTitleCount'	,
+					(	SELECT  COUNT(`sound_b`.`soundSubTitle`)
+						FROM	`comptonTransAnlys`.`sound` `sound_b` 
+						WHERE	1
+						AND		`sound_a`.`layoutName`		= `sound_b`.`layoutName`
+						AND		`sound_a`.`soundTitle`		= `sound_b`.`soundTitle`
+						AND		`sound_a`.`soundSubTitle`	= `sound_b`.`soundSubTitle`
+						GROUP BY	`sound_b`.`soundTitle`
+                        ,			`sound_b`.`soundSubTitle`
+					) 'COUNT'
+, 'soundPhoneme'	,	`sound_a`.`soundPhoneme`
+, 'contextPosition'	,	`context_a`.`contextPosition`
+, 'contextPositionCOUNT', 
+							(	SELECT  COUNT(`context_b`.`contextPosition`)
+								FROM	`comptonTransAnlys`.`sound`		`sound_b`
+								,   `comptonTransAnlys`.`context`	`context_b`
+								WHERE 1
+								AND	`sound_a`.`layoutName`		= `sound_b`.`layoutName`
+								AND `sound_a`.`soundAutoIncr`	= `sound_b`.`soundAutoIncr`
+								AND `sound_b`.`soundAutoIncr`	= `context_b`.`soundAutoIncr`
+								GROUP BY	`sound_b`.`soundTitle`
+								,			sound_b.soundSubTitle
+                                ,			sound_b.soundPhoneme  
+							) 'COUNT'
+, 'contextAutoIncr'	,	`context_a`.`contextAutoIncr`
+#) ''
+FROM	`comptonTransAnlys`.`sound`		`sound_a`
+,		`comptonTransAnlys`.`context`   `context_a`
 WHERE 1
-AND sound.soundAutoIncr = context.soundAutoIncr
-AND sound.layoutName = @LAYOUT_NAME
-ORDER BY sound.soundOrder
-, context.contextLabelOrder
+AND `sound_a`.`layoutName` = @LAYOUT_NAME
+AND `sound_a`.`soundAutoIncr` = `context_a`.`soundAutoIncr`
+ORDER BY	`sound_a`.`soundOrder`
+,			`context_a`.`contextLabelOrder`
 ;
