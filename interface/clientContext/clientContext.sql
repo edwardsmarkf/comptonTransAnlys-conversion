c89134390da21cd6ea3dc124dc118076d8b959de
marky edwards <mark@edwardsmark.com>
2022-08-01 13:53:21 -0700

    Update clientContext.sql

--- interface/clientContext/clientContext.sql
+++ interface/clientContext/clientContext.sql
@@ -1,16 +1,26 @@
+/*   clientContext.sql
+*/
         SET  @LAYOUT_NAME           = 'PESL'                            ;
         SET  @SESSION_NAME          = 'Time1'                           ;
         SET  @TEACHER_EMAIL         = 'info@englishwithoutaccent.com'   ;
-        SET  @CLIENT_EMAIL          = 'mark_f_edwards@yahoo.com'        ;   ## '12yukos@gmail.com'
+        SET  @CLIENT_EMAIL          = '12yukos@gmail.com'        		;   ## 'mark_f_edwards@yahoo.com'
         SET  @LANGUAGE_NORMS_NAME   = 'Indian-pakistan'                 ;
         SET  @STIMWORD_PAGE_NBR     =  "1"                              ;
         SET  @STIMWORD_LINE_NBR     =  "1"                              ;
         
-SELECT * 
-FROM comptonTransAnlys.clientContext
+SELECT  ##JSON_ARRAYAGG(
+                JSON_OBJECT
+( 'soundPhoneme'              ,    `clientContext`.`soundPhoneme`
+, 'clientContextError'        ,    `clientContext`.`clientContextSpeakingErrors`
+, 'frequency'                 ,    `clientContext`.`frequency`
+, 'clientContextErrorNotes'   ,    `clientContext`.`clientContextErrorNotes`
+, 'contextAutoIncr'           ,    `clientContext`.`contextAutoIncr`
+)
+FROM `comptonTransAnlys`.`clientContext`
 WHERE 1
-AND layoutName         = @LAYOUT_NAME
-AND teacherEmail       = @TEACHER_EMAIL
-AND clientMasterEmail  = @CLIENT_EMAIL
-AND sessionName        = @SESSION_NAME
+AND `layoutName`         = @LAYOUT_NAME
+AND `teacherEmail`       = @TEACHER_EMAIL
+AND `clientMasterEmail`  = @CLIENT_EMAIL
+AND `sessionName`        = @SESSION_NAME
+ORDER BY `contextAutoIncr`
 ;
