5ecb7f4f436124c6a70f99d5037f4464d8b282d4
marky edwards <mark@edwardsmark.com>
2022-08-02 09:26:01 -0700

    Update clientContext.sql

--- interface/clientContext/clientContext.sql
+++ interface/clientContext/clientContext.sql
@@ -1,23 +1,39 @@
 /*   clientContext.sql
 */
-        SET  @LAYOUT_NAME           = 'PESL'                            ;
-        SET  @SESSION_NAME          = 'Time1'                           ;
-        SET  @TEACHER_EMAIL         = 'info@englishwithoutaccent.com'   ;
-        SET  @CLIENT_EMAIL          =  'edwards.mark@yahoo.com'   ;   ##   '12yukos@gmail.com' ;   ## 'mark_f_edwards@yahoo.com
-        
-SELECT  ##JSON_ARRAYAGG(
+SET  @LAYOUT_NAME           = 'PESL'                            ;
+SET  @SESSION_NAME          = 'Time1'                           ;
+SET  @TEACHER_EMAIL         = 'info@comptonpeslonline.com'   ;
+SET  @CLIENT_EMAIL          = 'accentreduction.online@gmail.com'  ;  #'12yukos@gmail.com'        		;   ## 'mark_f_edwards@yahoo.com'
+     
+SELECT   ##JSON_ARRAYAGG(
                 JSON_OBJECT
-( 'soundPhoneme'              ,    `clientContext`.`soundPhoneme`
-, 'clientContextError'        ,    `clientContext`.`clientContextSpeakingErrors`
-, 'frequency'                 ,    `clientContext`.`frequency`
-, 'clientContextErrorNotes'   ,    `clientContext`.`clientContextErrorNotes`
-, 'contextAutoIncr'           ,    `clientContext`.`contextAutoIncr`
-) ''  ## blank out the header
+( 'soundPhoneme'                    ,    `clientContext`.`soundPhoneme`
+###, 'Occurences'                      ,    COUNT(`clientStimword`.`clientContextAutoIncr`)
+, 'clientContextError'              ,    `clientContext`.`clientContextError`
+, 'clientContextSpeakingsError'     ,    `clientContext`.`clientContextSpeakingErrors`
+, 'frequency'                       ,    `clientContext`.`frequency`
+, 'clientContextSpeakingsError'     ,    `clientContext`.`clientContextSpeakingErrors`
+, 'clientContextErrorNotes'         ,    `clientContext`.`clientContextErrorNotes`
+, 'contextAutoIncr'                 ,    `clientContext`.`contextAutoIncr`
+, 'clientContextAutoIncr'           ,    `clientContext`.`clientContextAutoIncr`
+#)
+) ''
 FROM `comptonTransAnlys`.`clientContext`
+#############################,    `comptonTransAnlys`.`stimwordPosition`
 WHERE 1
-AND `layoutName`         = @LAYOUT_NAME
-AND `teacherEmail`       = @TEACHER_EMAIL
-AND `clientMasterEmail`  = @CLIENT_EMAIL
-AND `sessionName`        = @SESSION_NAME
+AND `clientContext`.`layoutName`         = @LAYOUT_NAME
+AND `clientContext`.`teacherEmail`       = @TEACHER_EMAIL
+AND `clientContext`.`clientMasterEmail`  = @CLIENT_EMAIL
+AND `clientContext`.`sessionName`        = @SESSION_NAME
+###########AND  `stimwordPosition`.`stimwordPositionAutoIncr`
 ORDER BY `contextAutoIncr`
+;
+
+SELECT COUNT(*)
+FROM `comptonTransAnlys`.`context`
+,    `comptonTransAnlys`.`stimwordPosition`
+where `context`.`soundPhoneme` = 'b'
+and `context`.`contextPosition` = 'final'
+AND `context`.`layoutName` = 'PESL'
+AND `context`.`contextAutoIncr` = stimwordPosition.contextAutoIncr
 ;
