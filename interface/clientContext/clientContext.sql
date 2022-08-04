b52876cb97a685d2d3dc88e33fa4b8c27071a66c
marky edwards <mark@edwardsmark.com>
2022-08-04 09:44:48 -0700

    Update clientContext.sql

--- interface/clientContext/clientContext.sql
+++ interface/clientContext/clientContext.sql
@@ -2,13 +2,13 @@
 */
 SET  @LAYOUT_NAME           = 'PESL'                            ;
 SET  @SESSION_NAME          = 'Time1'                           ;
-SET  @TEACHER_EMAIL         = 'info@comptonpeslonline.com'   ;
-SET  @CLIENT_EMAIL          = 'accentreduction.online@gmail.com'  ;  #'12yukos@gmail.com'        		;   ## 'mark_f_edwards@yahoo.com'
-     
+SET  @TEACHER_EMAIL         = 'info@englishwithoutaccent.com'   ;
+SET  @CLIENT_EMAIL          = '12yukos@gmail.com'  ;  #'12yukos@gmail.com'        		;   ## 'mark_f_edwards@yahoo.com'
+
 SELECT   ##JSON_ARRAYAGG(
                 JSON_OBJECT
 ( 'soundPhoneme'                    ,    `clientContext`.`soundPhoneme`
-###, 'Occurences'                      ,    COUNT(`clientStimword`.`clientContextAutoIncr`)
+, 'Occurences'                      ,    COUNT(`stimwordPosition`.`contextAutoIncr`)
 , 'clientContextError'              ,    `clientContext`.`clientContextError`
 , 'clientContextSpeakingsError'     ,    `clientContext`.`clientContextSpeakingErrors`
 , 'frequency'                       ,    `clientContext`.`frequency`
@@ -19,16 +19,20 @@ SELECT   ##JSON_ARRAYAGG(
 #)
 ) ''
 FROM `comptonTransAnlys`.`clientContext`
-#############################,    `comptonTransAnlys`.`stimwordPosition`
+,    `comptonTransAnlys`.`context`
+,    `comptonTransAnlys`.`stimwordPosition`
 WHERE 1
-AND `clientContext`.`layoutName`         = @LAYOUT_NAME
-AND `clientContext`.`teacherEmail`       = @TEACHER_EMAIL
-AND `clientContext`.`clientMasterEmail`  = @CLIENT_EMAIL
-AND `clientContext`.`sessionName`        = @SESSION_NAME
-###########AND  `stimwordPosition`.`stimwordPositionAutoIncr`
-ORDER BY `contextAutoIncr`
+AND `context`.`layoutName`                     = @LAYOUT_NAME
+AND `clientContext`.`layoutName`               = @LAYOUT_NAME
+AND `clientContext`.`teacherEmail`             = @TEACHER_EMAIL
+AND `clientContext`.`clientMasterEmail`        = @CLIENT_EMAIL
+AND `clientContext`.`sessionName`              = @SESSION_NAME
+AND `context`.`contextAutoIncr`                = `stimwordPosition`.`contextAutoIncr`
+AND `context`.`contextAutoIncr`                = `clientContext`.`contextAutoIncr`
+GROUP BY ( `stimwordPosition`.`contextAutoIncr` )
+ORDER BY `clientContext`.`contextAutoIncr`
 ;
-
+/*
 SELECT COUNT(*)
 FROM `comptonTransAnlys`.`context`
 ,    `comptonTransAnlys`.`stimwordPosition`
@@ -36,4 +40,4 @@ where `context`.`soundPhoneme` = 'b'
 and `context`.`contextPosition` = 'final'
 AND `context`.`layoutName` = 'PESL'
 AND `context`.`contextAutoIncr` = stimwordPosition.contextAutoIncr
-;
+;*/
