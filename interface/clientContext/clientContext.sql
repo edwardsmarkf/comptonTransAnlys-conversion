62b42715f43f9e450d0f18f90a0283fdf4064937
marky edwards <mark@edwardsmark.com>
2022-08-06 09:35:28 -0700

    Update clientContext.sql

--- interface/clientContext/clientContext.sql
+++ interface/clientContext/clientContext.sql
@@ -1,5 +1,6 @@
 /*   clientContext.sql
 */
+
 SET  @LAYOUT_NAME           = 'PESL'                            ;
 SET  @SESSION_NAME          = 'Time1'                           ;
 SET  @TEACHER_EMAIL         = 'info@englishwithoutaccent.com'   ;
@@ -7,25 +8,17 @@ SET  @CLIENT_EMAIL          = '12yukos@gmail.com'  ;  #'12yukos@gmail.com'      
 
 SELECT   ##JSON_ARRAYAGG(
                 JSON_OBJECT
-( 'soundPhoneme'                    ,    `clientContext`.`soundPhoneme`
-, 'Occurences'                      ,    `context`.`contextCount`
-, 'clientContextError'              ,    `clientContext`.`clientContextError`
-, 'clientContextSpeakingsError'     ,    `clientContext`.`clientContextSpeakingErrors`
-, 'frequency'                       ,    `clientContext`.`frequency`
-, 'clientContextSpeakingsError'     ,    `clientContext`.`clientContextSpeakingErrors`
-, 'clientContextErrorNotes'         ,    `clientContext`.`clientContextErrorNotes`
-, 'contextAutoIncr'                 ,    `clientContext`.`contextAutoIncr`
-, 'clientContextAutoIncr'           ,    `clientContext`.`clientContextAutoIncr`
+(   'soundPhoneme'                 ,    `clientContext`.`soundPhoneme`
+,   'contextPosition'              ,    `clientContext`.`contextPosition`
+,  	'clientContextError'           ,   `clientContext`.`clientContextError`
+,   'clientContextSpeakingErrors'  ,   `clientContext`.`clientContextSpeakingErrors`
+,   'clientContextErrorNotes'      ,   `clientContext`.`clientContextErrorNotes`
 #)
 ) ''
 FROM `comptonTransAnlys`.`clientContext`
-,    `comptonTransAnlys`.`context`
 WHERE 1
-AND `context`.`layoutName`                     = @LAYOUT_NAME
 AND `clientContext`.`layoutName`               = @LAYOUT_NAME
 AND `clientContext`.`teacherEmail`             = @TEACHER_EMAIL
 AND `clientContext`.`clientMasterEmail`        = @CLIENT_EMAIL
 AND `clientContext`.`sessionName`              = @SESSION_NAME
-AND `context`.`contextAutoIncr`                = `clientContext`.`contextAutoIncr`
-ORDER BY `clientContext`.`contextAutoIncr`
 ;
