f5690d5d0a325dd832c4dd73658ed0a46bcf12fb
marky edwards <mark@edwardsmark.com>
2022-08-06 09:50:42 -0700

    Update clientContext.sql

--- interface/clientContext/clientContext.sql
+++ interface/clientContext/clientContext.sql
@@ -1,6 +1,7 @@
 /*   clientContext.sql
 */
 
+
 SET  @LAYOUT_NAME           = 'PESL'                            ;
 SET  @SESSION_NAME          = 'Time1'                           ;
 SET  @TEACHER_EMAIL         = 'info@englishwithoutaccent.com'   ;
@@ -8,15 +9,16 @@ SET  @CLIENT_EMAIL          = '12yukos@gmail.com'  ;  #'12yukos@gmail.com'      
 
 SELECT   ##JSON_ARRAYAGG(
                 JSON_OBJECT
-(   'soundPhoneme'                 ,    `clientContext`.`soundPhoneme`
-,   'contextPosition'              ,    `clientContext`.`contextPosition`
-,  	'clientContextError'           ,   `clientContext`.`clientContextError`
+(   'contextAutoIncr'              ,    `clientContext`.`contextAutoIncr`
+,	'clientContextError'           ,   `clientContext`.`clientContextError`
 ,   'clientContextSpeakingErrors'  ,   `clientContext`.`clientContextSpeakingErrors`
 ,   'clientContextErrorNotes'      ,   `clientContext`.`clientContextErrorNotes`
 #)
 ) ''
-FROM `comptonTransAnlys`.`clientContext`
+FROM `comptonTransAnlys`.`context`
+,    `comptonTransAnlys`.`clientContext`
 WHERE 1
+AND `context`.`contextAutoIncr`     = `clientContext`.`contextAutoIncr`
 AND `clientContext`.`layoutName`               = @LAYOUT_NAME
 AND `clientContext`.`teacherEmail`             = @TEACHER_EMAIL
 AND `clientContext`.`clientMasterEmail`        = @CLIENT_EMAIL
