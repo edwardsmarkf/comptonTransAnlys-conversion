8bff4fedb9da2be5deccc1ff85860f2ec8a61ee8
marky edwards <mark@edwardsmark.com>
2022-08-06 09:23:39 -0700

    Create transcriptionDisplay.sql

---
+++ interface/transcriptionDisplay/transcriptionDisplay.sql
@@ -0,0 +1,31 @@
+/*   transcriptionDisplay.sql
+*/
+SET  @LAYOUT_NAME           = 'PESL'                            ;
+SET  @SESSION_NAME          = 'Time1'                           ;
+SET  @TEACHER_EMAIL         = 'info@englishwithoutaccent.com'   ;
+SET  @CLIENT_EMAIL          = '12yukos@gmail.com'  ;  #'12yukos@gmail.com'        		;   ## 'mark_f_edwards@yahoo.com'
+
+SELECT   ##JSON_ARRAYAGG(
+                JSON_OBJECT
+( 'soundPhoneme'                    ,    `clientContext`.`soundPhoneme`
+, 'Occurences'                      ,    `context`.`contextCount`
+, 'clientContextError'              ,    `clientContext`.`clientContextError`
+, 'clientContextSpeakingsError'     ,    `clientContext`.`clientContextSpeakingErrors`
+, 'frequency'                       ,    `clientContext`.`frequency`
+, 'clientContextSpeakingsError'     ,    `clientContext`.`clientContextSpeakingErrors`
+, 'clientContextErrorNotes'         ,    `clientContext`.`clientContextErrorNotes`
+, 'contextAutoIncr'                 ,    `clientContext`.`contextAutoIncr`
+, 'clientContextAutoIncr'           ,    `clientContext`.`clientContextAutoIncr`
+#)
+) ''
+FROM `comptonTransAnlys`.`clientContext`
+,    `comptonTransAnlys`.`context`
+WHERE 1
+AND `context`.`layoutName`                     = @LAYOUT_NAME
+AND `clientContext`.`layoutName`               = @LAYOUT_NAME
+AND `clientContext`.`teacherEmail`             = @TEACHER_EMAIL
+AND `clientContext`.`clientMasterEmail`        = @CLIENT_EMAIL
+AND `clientContext`.`sessionName`              = @SESSION_NAME
+AND `context`.`contextAutoIncr`                = `clientContext`.`contextAutoIncr`
+ORDER BY `clientContext`.`contextAutoIncr`
+;
