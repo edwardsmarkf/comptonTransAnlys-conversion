a93ba36ce374b7d671c3c611bc1d7f703a93658a
marky edwards <mark@edwardsmark.com>
2022-08-06 09:57:56 -0700

    Update clientContext.sql

--- interface/clientContext/clientContext.sql
+++ interface/clientContext/clientContext.sql
@@ -10,17 +10,27 @@ SET  @CLIENT_EMAIL          = '12yukos@gmail.com'  ;  #'12yukos@gmail.com'      
 SELECT   ##JSON_ARRAYAGG(
                 JSON_OBJECT
 (   'contextAutoIncr'              ,    `clientContext`.`contextAutoIncr`
-,	'clientContextError'           ,   `clientContext`.`clientContextError`
+,	  'clientContextError'           ,   `clientContext`.`clientContextError`
 ,   'clientContextSpeakingErrors'  ,   `clientContext`.`clientContextSpeakingErrors`
 ,   'clientContextErrorNotes'      ,   `clientContext`.`clientContextErrorNotes`
 #)
 ) ''
 FROM `comptonTransAnlys`.`context`
 ,    `comptonTransAnlys`.`clientContext`
+,   `comptonTransAnlys`.`clientSession`
+,   `comptonTransAnlys`.`clientMaster`
 WHERE 1
-AND `context`.`contextAutoIncr`     = `clientContext`.`contextAutoIncr`
+AND   `context`.`contextAutoIncr`               = `clientContext`.`contextAutoIncr`
+AND   `clientContext`.`clientSessionAutoIncr`   =  `clientSession`.`clientSessionAutoIncr`
+AND   `clientContext`.`clientMasterEmail`       = `clientSession`.`clientMasterEmail`
+
+AND   `clientSession`.`clientMasterAutoIncr`    = `clientMaster`.`clientMasterAutoIncr`
+AND   `clientSession`.`clientMasterEmail`       = `clientMaster`.`clientMasterEmail`
+
 AND `clientContext`.`layoutName`               = @LAYOUT_NAME
 AND `clientContext`.`teacherEmail`             = @TEACHER_EMAIL
-AND `clientContext`.`clientMasterEmail`        = @CLIENT_EMAIL
+AND `clientMaster`.`clientMasterEmail`         = @CLIENT_EMAIL
 AND `clientContext`.`sessionName`              = @SESSION_NAME
+
+
 ;
