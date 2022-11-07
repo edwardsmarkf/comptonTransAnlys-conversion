f46989a52b09711208042eccef5d84ae31a7804f
marky edwards <mark@edwardsmark.com>
2022-11-07 10:36:58 -0700

    Update clientContext.sql

--- interface/clientContext/clientContext.sql
+++ interface/clientContext/clientContext.sql
@@ -1,20 +1,24 @@
 /*   clientContext.sql
+      2022-11-07  - added count of clientStimword to determine if this row is eligible for a key change
+                      if greater than zero, then there is a corresponding clientStimword record, and we are not allowed
+                      to change the key value
+*/
+/*   clientContext.sql
 */
 SET  @LAYOUT_NAME           = 'PESL'                            ;
 SET  @SESSION_NAME          = 'Time1'                           ;
 SET  @TEACHER_EMAIL         = 'info@englishwithoutaccent.com'   ;
-SET  @CLIENT_EMAIL          = '12yukos@gmail.com'  ;  #'12yukos@gmail.com'        		;   ## 'mark_f_edwards@yahoo.com'
+SET  @CLIENT_EMAIL          = '12yukos@gmail.com'  ;  #'12yukos@gmail.com'                      ;   ## 'mark_f_edwards@yahoo.com'
 
 SELECT   ##JSON_ARRAYAGG(
                 JSON_OBJECT
 ( 'soundPhoneme'                    ,    `clientContext`.`soundPhoneme`
-############, 'Occurences'                      ,    `context`.`contextCount`
 , 'clientContextError'              ,    `clientContext`.`clientContextError`
-############, 'frequency'                       ,    `clientContext`.`frequency`
 , 'clientContextSpeakingErrors'     ,    `clientContext`.`clientContextSpeakingErrors`
 , 'clientContextErrorNotes'         ,    `clientContext`.`clientContextErrorNotes`
 , 'contextAutoIncr'                 ,    `clientContext`.`contextAutoIncr`
 , 'clientContextAutoIncr'           ,    `clientContext`.`clientContextAutoIncr`
+, 'clientStimwordCounting'                  ,    COUNT( `clientStimword`.`clientStimwordAutoIncr`)
 #)
 ) ''
 FROM `comptonTransAnlys`.`context`
@@ -23,6 +27,7 @@ FROM `comptonTransAnlys`.`context`
 ,    `comptonTransAnlys`.`clientMaster`
 ,    `comptonTransAnlys`.`clientSession`
 ,    `comptonTransAnlys`.`clientContext`
+                LEFT JOIN `clientStimword` ON   `clientContext`.`clientContextAutoIncr` = `clientStimword`.`clientContextAutoIncr`
 WHERE 1
 AND `context`.`layoutName`                     = @LAYOUT_NAME
 AND `layout`.`layoutName`                      = @LAYOUT_NAME
@@ -31,7 +36,6 @@ AND `clientMaster`.`clientMasterEmail`         = @CLIENT_EMAIL
 AND `clientSession`.`sessionName`              = @SESSION_NAME
 
 AND  `layout`.`layoutName`                     = `teacher`.`layoutName`
-#####  ????  ######  AND  `layout`.`layoutAutoIncr`                 = `teacher`.`layoutAutoIncr`
 
 AND `teacher`.`teacherEmail`                   = `clientMaster`.`teacherEmail`
 AND `teacher`.`teacherAutoIncr`                = `clientMaster`.`teacherAutoIncr`
@@ -46,5 +50,10 @@ AND `clientSession`.`sessionName`             = `clientContext`.`sessionName`
 AND `clientSession`.`clientSessionAutoIncr`   = `clientContext`.`clientSessionAutoIncr`
 
 AND `context`.`contextAutoIncr`                = `clientContext`.`contextAutoIncr`
+                                                                                                GROUP BY `clientContext`.`clientContextAutoIncr`
 ORDER BY `clientContext`.`contextAutoIncr`
 ;
+
+############, 'Occurences'                      ,    `context`.`contextCount`
+############, 'frequency'                       ,    `clientContext`.`frequency`
+#####  ????  ######  AND  `layout`.`layoutAutoIncr`                 = `teacher`.`layoutAutoIncr`
