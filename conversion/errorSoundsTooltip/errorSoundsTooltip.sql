0493c7e6d379c91c4c7f8e97aff604a7c5b2f552
marky edwards <mark@edwardsmark.com>
2023-01-15 08:45:24 -0700

    Update errorSoundsTooltip.sql

--- conversion/errorSoundsTooltip/errorSoundsTooltip.sql
+++ conversion/errorSoundsTooltip/errorSoundsTooltip.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS `comptonTransAnlys`.`errorSoundsTooltip` ;
 
 CREATE TABLE  `comptonTransAnlys`.`errorSoundsTooltip`	
-(	`layoutName`          CHAR(4)                       NOT NULL
-, `errorSound`	        CHAR(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT ''
-,	`rowNbr`			        SMALLINT	                    NOT NULL
-,	`toolTip`			        VARCHAR(50)	                  NULL
-,	`errorSoundsAutoIncr`	SMALLINT	                    NOT NULL AUTO_INCREMENT
+(	`layoutName`                  CHAR(4)                       NOT NULL
+, `errorSound`	                CHAR(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT ''
+,	`rowNbr`			                SMALLINT	                    NOT NULL
+,	`toolTip`			                VARCHAR(50)	                  NULL
+,	`errorSoundsTooltipAutoIncr`	SMALLINT	                    NOT NULL AUTO_INCREMENT
 ,	PRIMARY KEY (`errorSoundsAutoIncr`)
 ); 
 
