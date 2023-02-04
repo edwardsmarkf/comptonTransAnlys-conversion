36655dbfbcfe352ed61eb44a28555903b0e23588
marky edwards <mark@edwardsmark.com>
2023-02-04 09:41:19 -0700

    Create clientContextTESTING.sql

---
+++ interface/clientContext/clientContextTESTING.sql
@@ -0,0 +1,15 @@
+
+                INSERT INTO clientContext
+                        SET     layoutName                      =       'PESL'
+                        ,       teacherEmail                    =       'info@englishwithoutaccent.com'
+                        ,       clientMasterEmail               =       '12yukos@gmail.com'
+                        ,       sessionName                     =       'Time2'
+						            ,       soundPhoneme                    =       'b'
+                        ,       contextPosition					        =		'initial'
+                        ,       clientContextError              =       'XX'
+                        ,		    frequency						            =		''
+                        ,       createdAt                       =       CURDATE()
+                        ,       updatedAt                       =       CURDATE()
+                        ,       contextAutoIncr                 =       6
+                        ,       clientSessionAutoIncr           =       2349
+                        ;
