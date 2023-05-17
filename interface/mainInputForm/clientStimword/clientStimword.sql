1eacf3aba914c1e6a8cbf4928202b710a6c95827
marky edwards <mark@edwardsmark.com>
2023-05-17 09:05:13 -0700

    Update clientStimword.sql

--- interface/mainInputForm/clientStimword/clientStimword.sql
+++ interface/mainInputForm/clientStimword/clientStimword.sql
@@ -1,6 +1,7 @@
        
        
-       update clientStimword set clientStimwordNotes = 'Manually added by mark!' 
+       update clientStimword 
+       set clientStimwordNotes = 'Manually added by mark!' 
        where clientMasterEmail = '12yukos@gmail.com' 
        and stimwordWord = 'crib'  
        and clientStimwordAutoIncr = 8109607
