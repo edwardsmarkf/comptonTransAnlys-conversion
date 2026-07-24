08ecce8e79dda1cd7d1227aa4470638973f94f1b
marky edwards <mark@edwardsmark.com>
2026-07-25 01:19:41 +0200

    Create clientContextStimwordPositionSelectSQL.sql

---
+++ feathers-server/sql/clientContextStimwordPositionSelectSQL.sql
@@ -0,0 +1,20 @@
+SELECT  JSON_ARRAYAGG
+        (       DISTINCT JSON_OBJECT
+                (       'stimwordWord'          ,       `stimword`.`stimwordWord`
+                ,       'stimwordAutoIncr'      ,       `stimword`.`stimwordAutoIncr`
+                )
+        ) 'JSON_ARRAYAGG'
+FROM    `clientContext`
+,       `clientStimword`
+,       `stimword`
+,       `stimwordPosition`
+WHERE   1
+AND     `clientContext`.`clientContextAutoIncr`         = `clientStimword`.`clientContextAutoIncr`
+AND     `stimwordPosition`.`stimwordPositionAutoIncr`   = `clientStimword`.`stimwordPositionAutoIncr`
+AND     `stimwordPosition`.`stimwordAutoIncr`           = `stimword`.`stimwordAutoIncr`
+AND     `clientContext`.`clientContextAutoIncr`         = :clientContextAutoIncr
+ORDER BY
+        `stimword`.`stimwordWord`
+        ASC
+;
+#
