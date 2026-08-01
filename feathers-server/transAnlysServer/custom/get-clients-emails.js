cd69d56d6f8ab0e4ff83752cb267c29afc272782
marky edwards <mark@edwardsmark.com>
2026-08-01 19:27:34 +0200

    Create get-clients-emails.js

---
+++ feathers-server/transAnlysServer/custom/get-clients-emails.js
@@ -0,0 +1,107 @@
+
+export funtion getClientsEmails {
+
+        const fs = require('fs');
+        const knexClient = this.app.get('knexClient');
+
+        const voiceFileDirectory        =       '/home/mark/voicefiles/'        ;
+
+
+                /* first look in the Mariadb clientMaster table */
+
+        let  clientMasterEmailJSON =  await knexClient.raw( clientMasterEmailSql , params.query   );
+
+        let clientMasterEmailObj        = JSON.parse(clientMasterEmailJSON[0][0]['JSON_OBJECTAGG']);
+        clientMasterEmailJSON           = null;
+
+        let clientMasterEmailKeys       = Object.keys(clientMasterEmailObj);
+
+
+
+                        /* now check the clients directory (email address) for any Eval_ voicefiles */
+
+        const rootDirName       =       voiceFileDirectory
+                        +       params.query['TEACHER_EMAIL']
+                        +       '/'
+                        ;
+
+        let directoryNames = [];
+                                //      https://www.geeksforgeeks.org/node-js-fs-readdirsync-method/
+        for ( const clientEmailObj of fs.readdirSync(rootDirName, { withFileTypes: true }) )    {
+                if  ( clientEmailObj.isDirectory() ) {  // isDirectory() seems to wrok with 'isFileTypes: true'  ( ;-)
+                        const clientDirectory = (rootDirName + '/' + clientEmailObj.name + '/' );
+                        if  (  fs.readdirSync(clientDirectory).filter(file => file.startsWith('Eval_')).length ) {
+                                                                                        // only look for Eval_ prefix
+                                directoryNames.push(clientEmailObj.name);
+                        }
+                }
+        }
+
+
+                        /* remove any client directory name (email) that has a corrsponding entry in the clientMaster table */
+
+                                //      https://www.techiedelight.com/find-difference-between-two-arrays-in-javascript/
+        let untranscribedEmail = directoryNames.filter(x => !clientMasterEmailKeys.includes(x));
+        directoryNames = null;  // free up a bit of space
+
+
+                        /* convert clientMasterEmail to an object for smart-html-elements   */
+
+        let clientMasterReturnObj = new Array();
+        for (const element of clientMasterEmailKeys.sort() )
+        {
+                clientMasterReturnObj.push      (       {       'group'         :       'Transcribed Emails'
+                                                        ,       'label'         :       element
+                                                        ,       'value'         :       JSON.stringify(
+                                                                                                {       'email'         :       element
+                                                                                                ,       'language'      :       clientMasterEmailObj[element]
+                                                                                                }
+                                                                                        )
+                                                        }
+                                                )
+        };
+        clientMasterEmailObj    = null;
+        clientMasterEmailKeys   = null;
+
+
+
+                        /* convert untranscribed emails to an object for smart-html-elements   */
+        let untranscribedEmailObj = new Array();
+        for (const element of untranscribedEmail.sort() )
+        {
+                untranscribedEmailObj.push      (       {       'group' :       'Untranscribed Emails'
+                                                        ,       'label' :       element
+                                                        ,       'value' :       JSON.stringify({ 'email' : element })
+                                                        }
+                                                )
+        };
+        untranscribedEmail = null;
+
+
+
+
+                        /* return the results */
+        let returnArray = [];
+        if  ( untranscribedEmailObj.length )    {
+                returnArray =   [       ...untranscribedEmailObj
+                                ,       ...clientMasterReturnObj
+                                ]
+                                ;
+        } else {
+                returnArray =   clientMasterReturnObj
+        }
+
+        return returnArray;
+  };
+};
+
+const clientMasterEmailSql =
+`
+       SELECT
+            JSON_OBJECTAGG(\`clientMaster\`.\`clientMasterEmail\`,  \`clientMaster\`.\`clientMasterSpecLanguage\`   ) 'JSON_OBJECTAGG'
+        FROM    \`clientMaster\`
+        WHERE   1
+        AND     \`layoutName\`          =  :LAYOUT_NAME
+        AND     \`teacherEmail\`        =  :TEACHER_EMAIL
+        ;
+`;
