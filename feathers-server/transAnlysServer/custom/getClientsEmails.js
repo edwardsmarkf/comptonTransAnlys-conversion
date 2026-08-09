f0b7a687eaa8292732cfcb4baf69701b41d1d27d
marky edwards <mark@edwardsmark.com>
2026-08-09 19:35:46 +0200

    Update getClientsEmails.js

--- feathers-server/transAnlysServer/custom/getClientsEmails.js
+++ feathers-server/transAnlysServer/custom/getClientsEmails.js
@@ -107,18 +107,3 @@ export class  getClientsEmails {
         return returnArray;
   };
 };
-
-
-
-
-
-//const clientMasterEmailSql =
-//`
-//       SELECT
-//            JSON_OBJECTAGG(\`clientMaster\`.\`clientMasterEmail\`,  \`clientMaster\`.\`clientMasterSpecLanguage\`   ) 'JSON_OBJECTAGG'
-//        FROM    \`clientMaster\`
-//        WHERE   1
-//        AND     \`layoutName\`          =  :LAYOUT_NAME
-//        AND     \`teacherEmail\`        =  :TEACHER_EMAIL
-//        ;
-`;
