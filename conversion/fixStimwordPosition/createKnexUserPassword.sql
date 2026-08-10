aa18cd565491e483b248d08d6cf17abc616c62e8
marky edwards <mark@edwardsmark.com>
2026-08-10 07:01:37 +0200

    Create createKnexUserPassword.sql

---
+++ conversion/fixStimwordPosition/createKnexUserPassword.sql
@@ -0,0 +1,15 @@
+DROP USER IF EXISTS 'knexUser'@'localhost'				                    ;
+CREATE USER 'knexUser'@'localhost' IDENTIFIED BY 'knexPassword'       ;
+
+FLUSH PRIVILEGES  ;
+
+GRANT ALL ON `comptonTransAnlys`.* TO 'knexUser'@'localhost'	                                        \
+IDENTIFIED BY 'knexPassword' 							                                                            \
+WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0	;
+
+############################### DROP DATABASE `comptonTransAnlys` 					;
+#################CREATE DATABASE IF NOT EXISTS `comptonTransAnlys` 			;
+
+GRANT ALL PRIVILEGES ON `comptonTransAnlys`.* TO 'knexUser'@'localhost'	        ;
+
+# mariadb  --host=localhost --user=knexUser  --password=knexPassword    comptonTransAnlys    ;   
