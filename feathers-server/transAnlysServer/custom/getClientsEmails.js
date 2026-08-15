c93d90116cde3b08d00cead04380b7d65fa2b0d3
marky edwards <mark@edwardsmark.com>
2026-08-15 19:23:46 +0200

    Update getClientsEmails.js

--- feathers-server/transAnlysServer/custom/getClientsEmails.js
+++ feathers-server/transAnlysServer/custom/getClientsEmails.js
@@ -3,9 +3,13 @@
 
 */
 
+/*
+    import { readFile } from 'fs/promises'  ;       // needed to read the directory structure.
+    import fs from 'fs'                     ;       // added 2026-08-15
+*/
+import fs, { readFile } from 'fs'; // Note: readFile here is callback-based, use fs.promises.readFile for promises (from  AI)
 
-import { readFile } from 'fs/promises'  ;       // needed to read the directory structure.
-import fs from 'fs'                     ;       // added 2026-08-15
+
 export class  getClientsEmails {
   constructor(app) {
     this.app = app;
